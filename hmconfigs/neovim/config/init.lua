61e3524a9286707cc85b4c2030d78bda89167c34
ext4 <ghaleb.sarsour2007@gmail.com>
2026-01-15 16:33:19 -0500

    added plugins to neovim and tmux

--- hmconfigs/neovim/config/init.lua
+++ hmconfigs/neovim/config/init.lua
@@ -17,6 +17,63 @@ vim.o.winborder = 'rounded'
 vim.o.signcolumn = 'yes'
 vim.o.termguicolors = true
 
+--Folding:
+vim.defer_fn(function()
+  vim.api.nvim_set_hl(0, "Folded", { fg = "white", bg = "darkblue" })
+end, 50)
+vim.o.foldenable = true
+vim.o.foldlevel = 99
+vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.o.foldtext = ""
+vim.opt.foldcolumn = "0"
+vim.opt.fillchars:append({ fold = " " })
+vim.o.foldmethod = 'expr'
+-- Default to treesitter folding
+vim.o.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+-- Prefer LSP folding if client supports it
+vim.api.nvim_create_autocmd('LspAttach', {
+	callback = function(args)
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		if client:supports_method('textDocument/foldingRange') then
+			local win = vim.api.nvim_get_current_win()
+			vim.wo[win][0].foldexpr = 'v:lua.vim.lsp.foldexpr()'
+		end
+	end,
+})
+
+
+-- Todo color highlighting
+local set_highlights = function()
+	-- Unchecked items (white text and icon)
+	vim.api.nvim_set_hl(0, 'RenderMarkdownUnchecked', {
+		fg = '#FFFFFF', ctermfg = 'White', bold = false
+	})
+	
+	-- Checked items (green text and icon)
+	vim.api.nvim_set_hl(0, 'RenderMarkdownChecked', {
+		fg = '#AEF0A3', ctermfg = 'Green', bold = false
+	})
+	
+	-- Custom status highlights
+	vim.api.nvim_set_hl(0, 'RenderMarkdownImportant', {
+		fg = '#F5011A', ctermfg = 'Red', bold = false
+	})
+	vim.api.nvim_set_hl(0, 'RenderMarkdownRecurring', {
+		fg = '#78A1F5', ctermfg = 'Blue', bold = false
+	})
+	vim.api.nvim_set_hl(0, 'RenderMarkdownInProgress', {
+		fg = '#F5F28B', ctermfg = 'Yellow', bold = false
+	})
+end
+
+-- Apply highlights immediately
+set_highlights()
+
+-- Re-apply when colorscheme changes
+vim.api.nvim_create_autocmd("ColorScheme", {
+	pattern = "*",
+	callback = set_highlights
+})
 
 -- Listchars
 vim.o.list = true
@@ -58,6 +115,8 @@ vim.pack.add({
 	{ src = "https://github.com/mikavilpas/yazi.nvim" },
 	{ src = "https://github.com/nvim-lua/plenary.nvim" },
 	{ src = "https://github.com/folke/snacks.nvim" },
+	{ src = "https://github.com/nvim-tree/nvim-web-devicons" },
+	{ src = "https://github.com/MeanderingProgrammer/render-markdown.nvim" },
 })
 
 require("oil").setup()
@@ -88,7 +147,50 @@ require("snacks").setup {
 		animate = { enabled = true },
 	},
 }
-
+require('render-markdown').setup {
+	completions = { lsp = { enabled = true } },
+	bullet = { icons = { '', '󰋙', '◆', '◇', '', '○' } },
+	heading = {
+		signs = { '󰫎', '󰫍' },
+		icons = { '󰎤 ', '󰎧 ', '󰎪 ', '󰎭 ', '󰎱 ', '󰎳 ' },
+		position = 'overlay',
+	},
+	checkbox = {
+		enabled = true,
+		render_modes = false,
+		right_pad = 1,
+		unchecked = {
+			icon = '󰄱 ',
+			highlight = 'RenderMarkdownUnchecked',
+			scope_highlight = 'RenderMarkdownUnchecked',
+		},
+		checked = {
+			icon = '󰱒 ',
+			highlight = 'RenderMarkdownChecked',
+			scope_highlight = 'RenderMarkdownChecked',
+		},
+		custom = {
+			important = {
+				raw = '[!]',
+				rendered = ' ',
+				highlight = 'RenderMarkdownImportant',
+				scope_highlight = 'RenderMarkdownImportant'
+			},
+			recurring = {
+				raw = '[R]',
+				rendered = ' ',
+				highlight = 'RenderMarkdownRecurring',
+				scope_highlight = 'RenderMarkdownRecurring'
+			},
+			in_progress = {
+				raw = '[P]',
+				rendered = ' ',
+				highlight = 'RenderMarkdownInProgress',
+				scope_highlight = 'RenderMarkdownInProgress'
+			},
+		},
+	},
+}
 
 -- Colorscheme
 vim.cmd("colorscheme moonfly")
